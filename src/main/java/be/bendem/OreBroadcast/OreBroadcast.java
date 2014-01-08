06eebf13ceee2d09fc9e17b821f2d1f32b130012
bendem <online@bendem.be>
2014-01-08 20:01:21 +0100

    Convert indentation to spaces

--- src/main/java/be/bendem/OreBroadcast/OreBroadcast.java
+++ src/main/java/be/bendem/OreBroadcast/OreBroadcast.java
@@ -12,24 +12,24 @@ import java.util.logging.Logger;
  */
 public class OreBroadcast extends JavaPlugin {
 
-	public PluginDescriptionFile pdfFile;
-	public Logger logger;
+    public PluginDescriptionFile pdfFile;
+    public Logger logger;
 
-	@Override
-	public void onEnable() {
-		logger = getLogger();
+    @Override
+    public void onEnable() {
+        logger = getLogger();
 
-		pdfFile = getDescription();
-		logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+        pdfFile = getDescription();
+        logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
 
-		getServer().getPluginManager().registerEvents(new BlockBreakListener(this), this);
-		getCommand("ob").setExecutor(new CommandHandler(this));
-		saveDefaultConfig();
-	}
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(this), this);
+        getCommand("ob").setExecutor(new CommandHandler(this));
+        saveDefaultConfig();
+    }
 
-	@Override
-	public void onDisable() {
-		logger.info(pdfFile.getName() + " want you to have a nice day ;-)");
-	}
+    @Override
+    public void onDisable() {
+        logger.info(pdfFile.getName() + " want you to have a nice day ;-)");
+    }
 
 }
