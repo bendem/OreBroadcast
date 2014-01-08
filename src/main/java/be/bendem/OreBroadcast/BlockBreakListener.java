8f9b18df5d34ced15cd57566df3f8da827bbcc19
bendem <online@bendem.be>
2014-01-08 16:33:53 +0100

    implementation started!

--- src/main/java/be/bendem/OreBroadcast/BlockBreakListener.java
+++ src/main/java/be/bendem/OreBroadcast/BlockBreakListener.java
@@ -4,6 +4,10 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import org.bukkit.GameMode;
+
+import java.util.List;
+
 public class BlockBreakListener implements Listener {
 
     public OreBroadcast plugin;
@@ -14,7 +18,26 @@ public class BlockBreakListener implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        plugin.logger.info("A block just broke...");
+        // Reject creative users
+        if(event.getPlayer().getGameMode() != GameMode.SURVIVAL) {
+            return;
+        }
+
+        plugin.logger.info(event.getBlock().getType().name());
+
+        List<String> blocksToBroadcast = plugin.getConfig().getStringList("Ores");
+
+        for (int i = 0; i < blocksToBroadcast.size(); ++i) {
+            blocksToBroadcast.set(i, blocksToBroadcast.get(i).toUpperCase() + "_ORE");
+            // If redstone is in the config, add glowing redstone as well
+            if(blocksToBroadcast.get(i) == "REDSTONE_ORE") {
+                blocksToBroadcast.add("GLOWING_REDSTONE_ORE");
+            }
+        }
+
+        if(blocksToBroadcast.contains(event.getBlock().getType().name())) {
+            plugin.logger.info("A block to broadcast broke...");
+        }
     }
 
 }
