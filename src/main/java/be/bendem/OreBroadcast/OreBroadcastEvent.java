1a11df0bddbac13b75a128b2cae8fa01aca9a7c8
Chris Naude <chris.naude.0@gmail.com>
2014-03-20 15:24:16 -0700

    Formatting changes. Added javadocs. Make event cancellable.

--- src/main/java/be/bendem/OreBroadcast/OreBroadcastEvent.java
+++ src/main/java/be/bendem/OreBroadcast/OreBroadcastEvent.java
@@ -1,34 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- *//*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package be.bendem.OreBroadcast;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 /**
+ * Broadcast event for oraBroadcast.
  *
  * @author cnaude
  */
-public class OreBroadcastEvent extends Event {
+public class OreBroadcastEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList(); 
     
-    private final String message;
-    private final Player player;
+    private String message;
+    private Player player;
+    private boolean cancelled;
 
     /**
      *
-     * @param message
-     * @param player
+     * @param message the formatted message
+     * @param player the player that triggered the event
      */
     public OreBroadcastEvent(String message, Player player) {
         this.message = message;
@@ -36,19 +29,39 @@ public class OreBroadcastEvent extends Event {
     }
 
     /**
+     * Get the formatted message.
      *
      * @return
      */
     public String getMessage() {
         return this.message;
     }
+    
+    /**
+     * Set the message.
+     *
+     * @param message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
        
     /**
+     * Get the player.
      *
      * @return
      */
     public Player getPlayer() {
         return this.player;
+    }
+    
+    /**
+     * Set the player.
+     *
+     * @param player
+     */
+    public void setPlayer(Player player) {
+        this.player = player;
     }
 
     /**
@@ -66,5 +79,15 @@ public class OreBroadcastEvent extends Event {
      */
     public static HandlerList getHandlerList() {
         return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+         return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean bln) {
+         this.cancelled = bln;
     }
 }
