78f414292c445aea4bd38dd8617360f8a61ffa1c
bendem <online@bendem.be>
2014-06-25 00:26:03 +0200

    Add broadcast-creative-placed-blocks to config
    Closes #11, I don't think people would find permission for this *that* useful

--- src/main/java/be/bendem/orebroadcast/BlockPlaceListener.java
+++ src/main/java/be/bendem/orebroadcast/BlockPlaceListener.java
@@ -1,5 +1,6 @@
 package be.bendem.orebroadcast;
 
+import org.bukkit.GameMode;
 import org.bukkit.block.Block;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.EventHandler;
@@ -14,11 +15,13 @@ public class BlockPlaceListener implements Listener {
     }
 
     @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        Block block = event.getBlock();
+    public void onBlockPlace(BlockPlaceEvent e) {
+        Block block = e.getBlock();
         if(plugin.isWhitelisted(block.getType())
                 && plugin.isWorldWhitelisted(block.getWorld().getName())
-                && !plugin.isBlackListed(block)) {
+                && !plugin.isBlackListed(block)
+                && (e.getPlayer().getGameMode() != GameMode.CREATIVE
+                    || !plugin.getConfig().getBoolean("broadcast-creative-placed-blocks", true))) {
             plugin.blackList(block);
         }
     }
