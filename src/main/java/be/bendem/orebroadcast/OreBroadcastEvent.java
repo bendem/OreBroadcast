93bce8149f92f43adfb8aecc04498503e016a9ce
bendem <online@bendem.be>
2014-06-24 23:11:03 +0200

    Rename player to source and deprecate setPlayer since it is 100% useless
    + Rewrite OreBroadcastEvent jd

--- src/main/java/be/bendem/orebroadcast/OreBroadcastEvent.java
+++ src/main/java/be/bendem/orebroadcast/OreBroadcastEvent.java
@@ -1,10 +1,11 @@
 package be.bendem.orebroadcast;
 
-import java.util.Set;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+
+import java.util.Set;
 
 /**
  * Broadcast event for OreBroadcast messages.
@@ -16,7 +17,7 @@ public class OreBroadcastEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
 
     private String message;
-    private Player player;
+    private Player source;
     private boolean cancelled = false;
     private Set<Player> recipients;
 
@@ -24,46 +25,46 @@ public class OreBroadcastEvent extends Event implements Cancellable {
      * Broadcast event for OreBroadcast messages.
      *
      * @param message the formatted message
-     * @param player the player that triggered the event
-     * @param recipients
+     * @param source the player that mined the block
+     * @param recipients the players that will receive this message
      */
-    public OreBroadcastEvent(String message, Player player, Set<Player> recipients) {
+    public OreBroadcastEvent(String message, Player source, Set<Player> recipients) {
         this.message = message;
-        this.player = player;
+        this.source = source;
         this.recipients = recipients;
     }
 
     /**
-     * Gets a set of recipients that this message will be displayed to
+     * Gets the recipients that this message will be sent to.
      *
-     * @return the players that receive this message
+     * @return the players that will receive this message
      */
     public Set<Player> getRecipients() {
         return recipients;
     }
 
     /**
-     * Sets of recipients that this message will be displayed to
+     * Sets the recipients that this message will be sent to.
      *
-     * @param recipients the players that receive this message
+     * @param recipients the players that will receive this message
      */
     public void setRecipients(Set<Player> recipients) {
         this.recipients = recipients;
     }
 
     /**
-     * Gets the formatted message.
+     * Gets the formatted message which will be sent.
      *
-     * @return
+     * @return the formatted message
      */
     public String getMessage() {
         return this.message;
     }
 
     /**
-     * Sets the message.
+     * Sets the message which will be sent.
      *
-     * @param message
+     * @param message the new message to set
      */
     public void setMessage(String message) {
         this.message = message;
@@ -73,18 +74,51 @@ public class OreBroadcastEvent extends Event implements Cancellable {
      * Gets the player.
      *
      * @return
+     * @deprecated Renamed, see {@link OreBroadcastEvent#getSource()}
      */
+    @Deprecated
     public Player getPlayer() {
-        return this.player;
+        return this.source;
     }
 
     /**
      * Sets the player.
      *
      * @param player
+     * @deprecated Removed, this had no effects anyway
      */
+    @Deprecated
     public void setPlayer(Player player) {
-        this.player = player;
+        throw new UnsupportedOperationException("This method is deprecated and does nothing!");
+    }
+
+    /**
+     * Gets the player who mined the block.
+     *
+     * @return the player who mined the block
+     */
+    public Player getSource() {
+        return this.source;
+    }
+
+    /**
+     * Gets the cancellation state of the event.
+     *
+     * @return true if the event is cancelled
+     */
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    /**
+     * Sets the cancellation state of the event
+     *
+     * @param cancel the new cancellation state
+     */
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
     }
 
     @Override
@@ -94,16 +128,6 @@ public class OreBroadcastEvent extends Event implements Cancellable {
 
     public static HandlerList getHandlerList() {
         return handlers;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        this.cancelled = cancel;
     }
 
 }
